41c14578784a568bec8968fcb80c0fc9c52d65bf
k-iatrou <158864273+k-iatrou@users.noreply.github.com>
2024-02-11 13:03:03 +0100

    4. Finished up project

--- script.py
+++ script.py
@@ -24,3 +24,12 @@ print(brownie_points)
 # creating dictionary that maps players to a list of the words they have played
 player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
 
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+
+print(player_to_points)
